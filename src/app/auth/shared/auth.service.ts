42bda7bdac1172222032df7a3db53ba9ac8fa0d1
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-05 18:10:57 +0530

    added signup

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { SignupRequestPayload } from '../signup/signup-request.payload';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
+    return this.httpClient.post(
+      'http://localhost:8080/api/auth/signup',
+      signupRequestPayload,
+      { responseType: 'text' }
+    );
+  }
 }
