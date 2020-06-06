ecd6fd2b7849cc0e1cd43e814115caf9d10f8cfb
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-06 22:56:31 +0530

    updated

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  goToCreatePost() {
+    this.router.navigateByUrl('/create-post');
   }
 
+  goToCreateSubreddit() {
+    this.router.navigateByUrl('/create-subreddit');
+  }
 }
